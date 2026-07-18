09b359c921e72b570d1e37c7b1a4496cfdfb90ac
zackkracky <48663430+zackkracky@users.noreply.github.com>
2026-07-19 01:50:00 +0530

    rlt logic intial stage

--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -1,17 +1,44 @@
 module uart_tx #(
-    parameter CLK_FREQ = 50_000_000;
-    parameter BAUD_RATE = 115200
-)(
-    input wire  clk,
-    input wire  rst_n,//active low
-    input wire  tx_start,
-    input wire  [7:0] tx_data,
+        parameter CLK_FREQ = 50_000_000;
+        parameter BAUD_RATE = 115200
+    )(
+        input wire  clk,
+        input wire  rst_n,//active low
+        input wire  tx_start,
+        input wire  [7:0] tx_data,
 
-    output reg  tx,
-    output reg  busy,
-    output reg  done
-);
+        output reg  tx,
+        output reg  busy,
+        output reg  done
+    );
 
-localparam integer CLKS_PER_BIT = CLK_FREQ / BAUD_RATE;
+    localparam integer CLKS_PER_BIT = CLK_FREQ / BAUD_RATE;
+    localparam IDLE = 2'b00; 
+    localparam START = 2'b01;
+    localparam DATA = 2'b10;
+    localparam STOP = 2'b11;
+
+    reg [1:0] state;
+    reg [15:0] baud_counter;
+    reg [2:0] bit_counter;
+    reg [7:0] shift_reg; 
+    
+    always @(posedge clk or negedge rst_n)begin
+
+        if (!rst_n) begin
+            state   <= IDLE;
+            baud_counter <= 16'd0;
+            bit_counter  <= 3'd0;
+            shift_reg    <= 8'd0;
+
+
+        end
+        else begin
+
+
+
+        end
+
+    end
 
 endmodule
